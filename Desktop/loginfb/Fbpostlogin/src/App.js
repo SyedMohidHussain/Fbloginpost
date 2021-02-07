82612826111e7a7056db384ec9aba8075ae2a04b
unknown <mohidhussaindot@gmail.com>
2021-02-07 07:24:07 +0500

    Create a React app and publish it to GitHub Pages

--- Desktop/loginfb/Fbpostlogin/src/App.js
+++ Desktop/loginfb/Fbpostlogin/src/App.js
@@ -6,10 +6,9 @@ import Page2 from "./Pages/Page2";
 function App() {
 	return (
 		<div className="App">
-			<Loginpage />
 			<Router>
 				<Switch>
-					{/* <Route path="/" exact component={Loginpage} /> */}
+					<Route path="/" exact component={Loginpage} />
 					<Route path="/page2" exact component={Page2} />
 				</Switch>
 			</Router>
